8e340ee272c257522e5e4437d14efe6babb3d5bf
komang234 <116166626+komang234@users.noreply.github.com>
2023-06-22 01:28:39 -0300

    Final peliculaSerieController.js

--- src/controllers/peliculaSerieController.js
+++ src/controllers/peliculaSerieController.js
@@ -1,64 +1,81 @@
-import PeliculaSerie from "../models/PeliculaSerie.js";
 import { Router } from 'express';
-import { Authenticate } from '../common/jwt.strategy.js';
-import { getSignedToken, getRandomString} from '../server.js'
-import { getByParams, getByID, create, deleteByID, update, getByIDSinUnion } from '../services/peliculaSerieService.js';
+import Pelicula from '../models/pelicula.js'; 
+import {createMovie, getAllMovies, filteredMovies, getDetailedMovie, updateMovie, deleteMovie} from '../services/peliculasService.js';
 
-const controller = Router()
+const router = Router();
 
-controller.get('/auth/login', async (req, res) => {
-    return res.status(200).json(getSignedToken());
-});
+router.get ('/movies', async(req, res)=>{
+    let status = 200;
+    const pelicula         = new Pelicula()
+    pelicula.Nombre        = req.query.name;
+    pelicula.Orden         = req?.query?.order?.toUpperCase();
+    let peliculas;
+    if(pelicula.Nombre || pelicula.Orden){
+        if(pelicula.Orden != "ASC" && pelicula.Orden != "DESC"){
+            status = 400;
+        }
+        else{
+            peliculas = await filteredMovies(pelicula);
+        }
+    }
+    else{
+        peliculas = await getAllMovies();
+    }
+    res.status(status).send(peliculas);
+})
+router.get ('/movies/:id', async(req, res)=>{ 
+    let status = 200;
+    const id               = req.params.id;
+    const pelicula         = await getDetailedMovie(id);
+    if(pelicula == null){
+        status = 404;
+    }
+    if (id < 0){
+        status = 400;
+    }
+    res.status(status).send(pelicula);
+})
+router.post ('/movies', async(req, res)=>{
+    let status = 201;
+    let creado;
+    const pelicula              = new Pelicula();
+    pelicula.Imagen             = req.body.Imagen;
+    pelicula.Titulo             = req.body.Titulo;
+    pelicula.FechaCreacion      = req.body.FechaCreacion;
+    pelicula.Calificacion       = req.body.Calificacion;
+    if(pelicula.Calificacion < 0 || pelicula.Calificacion > 5){
+        status = 400;
+    }
+    else{
+        creado = await createMovie(pelicula);
+        if(creado==null){
+            status = 400;
+        }
+    }
+    res.status(status).send(creado);
+})
+router.put ('/movies/:id', async(req, res)=>{
+    let status = 200;
+    const id                    = req.params.id;
+    const pelicula              = new Pelicula();
+    pelicula.Imagen             = req.body.Imagen;
+    pelicula.Titulo             = req.body.Titulo;
+    pelicula.FechaCreacion      = req.body.FechaCreacion;
+    pelicula.Calificacion       = req.body.Calificacion;
 
- controller.get('', Authenticate, async (req, res) => {
-    const name = req.query.name
-    const order = req.query.order
-
-    const peliculaSerie = await getByParams(name,order)
-
-    return res.status(200).json(peliculaSerie);
-}); 
-
-
-controller.get('/:id', Authenticate, async (req, res) => {
-    console.log(req.params.id)
-    const peliculaSerie = await getByID(req.params.id);
-    return res.status(200).json(peliculaSerie)
+    const cambiado      = await updateMovie(pelicula, id);
+    if(req.params.id < 0 || cambiado == null){
+        status = 400;
+    }
+    res.status(status).send(cambiado);
+})
+router.delete ('/movies/:id', async(req, res)=>{
+    let status = 200;
+    if(req.params.id < 0){
+        status = 400;
+    }
+    const idBorrado     = await deleteMovie(req.params.id);
+    res.status(status).send(idBorrado);
 })
 
-controller.post('', Authenticate, async (req, res)=> {
-    const peliculaSerie = new PeliculaSerie()
-
-    peliculaSerie.imagen = req.body.imagen
-    peliculaSerie.titulo = req.body.titulo
-    peliculaSerie.fechaCreacion = req.body.fechaCreacion
-    peliculaSerie.calificacion = req.body.calificacion
-    await create(peliculaSerie)
-    return res.status(201).json(peliculaSerie)
-})
-
-controller.delete('', Authenticate, async (req, res) => {
-    let peliculaSerie2 = new PeliculaSerie()
-    const id = req.body.id
-    peliculaSerie2 = await getByIDSinUnion(id);
-    console.log(peliculaSerie2)
-    await deleteByID(id);
-    return res.status(200).json(peliculaSerie2)
-})
-
-controller.put('', Authenticate, async (req, res) => {
-    const id = req.body.id
-    let peliculaSerie3 = new PeliculaSerie()
-    peliculaSerie3 = await getByID(id)
-    const peliculaSerie2 = new PeliculaSerie()
-    peliculaSerie2.imagen = req.body.imagen
-    peliculaSerie2.titulo = req.body.titulo
-    peliculaSerie2.fechaCreacion = req.body.fechaCreacion
-    peliculaSerie2.calificacion = req.body.calificacion
-    peliculaSerie3 = {...peliculaSerie3, ...peliculaSerie2}
-    await update(id, peliculaSerie3)
-    return res.status(200).json(peliculaSerie3)
-})
-
-
-export default controller;
+export default router;
