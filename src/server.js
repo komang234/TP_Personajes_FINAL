ce2603df5dcd7d4c444bd7470c1da6c7e143813f
komang234 <116166626+komang234@users.noreply.github.com>
2023-06-22 01:21:48 -0300

    Final server.js

--- src/server.js
+++ src/server.js
@@ -1,57 +1,20 @@
-import express, { Router } from "express";
-import jwt from "jsonwebtoken";
-import PersonajeRouter from "./controllers/personajesController.js";
-import PeliculaSerieRouter from "./controllers/peliculaSerieController.js";
-import 'dotenv/config';
+import express from "express";
 import passport from "passport";
-import { jwtStrategy } from "./common/jwt.strategy.js";
-
+import personajesController from "./controllers/personajesController.js";
+import peliculasController from "./controllers/peliculasController.js";
+import {jwtStrategy} from "./common/jwt.strategy.js"
+import auth from "./controllers/auth.js"
 const app = express();
-const port = 5000;
+const port = 3001;
+app.use(express.json());
+
 passport.use(jwtStrategy);
 app.use(passport.initialize());
 
-app.use(express.json());
+app.use("", auth)
+app.use("", personajesController)
+app.use("", peliculasController)
 
-export const getRandomString = () => {
-    var result = "";
-    var characters =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    var charactersLength = characters.length;
-    for (var i = 0; i < 18; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-  
-    return result;
-  };
-
-export const getSignedToken = () => {
-    const userId = getRandomString();
-    const userMail = `${userId}@personajes.com`;
-    const token = jwt.sign(
-        {
-            payload: "PersoMovie",
-            userEmail: userMail,
-        },
-        process.env.AUTH_HS256_KEY,
-        {
-            issuer: process.env.AUTH_ISSUER_URL,
-            subject: userId,
-            audience: ["http://localhost/"],
-            expiresIn: 60 * 60 * 24 * 5
-        }
-    );
-
-    return token;
-};
-
-
-app.use("/characters", PersonajeRouter)
-app.use("/movies", PeliculaSerieRouter)
-
-
-
-
-app.listen(port, () => {
-    console.log(`Se esta usando el puerto: ${port}`)
+app.listen (port, ()=>{
+    console.log(`EJEMPLO ${port}`)
 })
