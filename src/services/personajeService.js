82ed4988fb19629c28c03d9beba8eedc917cacdd
komang234 <116166626+komang234@users.noreply.github.com>
2023-06-22 01:23:37 -0300

    Final personajeService.js

--- src/services/personajeService.js
+++ src/services/personajeService.js
@@ -1,103 +1,67 @@
-import Personaje from "../models/Personaje.js";
-import sql from 'mssql'
-import configDB from "../models/db.js";
+import sql from 'mssql';
+import configDB from '../models/db.js';
 
+export const filteredCharacters = async (personaje) => {
+    let conn = await sql.connect(configDB);
+    let results;
+    let query;
 
-export const getByParams = async (nombre,edad,movie) => {
-    const conn = await sql.connect(configDB)
-    let results = 0
-    if(nombre)
-    {
-        if(edad)
-        {
-            if(movie)
-            {
-                results = await conn.request().input("whereCondition", nombre).input("whereCondition2", edad).input("whereCondition3", movie).query(`SELECT p.Id, p.Imagen, p.Nombre FROM Personaje as p INNER JOIN PersonajeXPeliculaSerie ON p.Id = PersonajeXPeliculaSerie.IdPersonaje INNER JOIN PeliculaSerie ON PersonajeXPeliculaSerie.IdPeliculaSerie = PeliculaSerie.Id WHERE p.Nombre LIKE '%${nombre}%' AND PersonajeXPeliculaSerie.IdPeliculaSerie = @whereCondition3 AND p.Edad = @whereCondition2`)
-            }
-            else{
-                results = await conn.request().input("whereCondition", nombre).input("whereCondition2", edad).query(`SELECT Id, Imagen, Nombre FROM Personaje WHERE Personaje.Nombre LIKE '%${nombre}%' AND Personaje.Edad = @whereCondition2`)
-            }
-        }
-        else{
-            if(movie)
-            {
-                results = await conn.request().input("whereCondition", nombre).input("whereCondition2", movie).query(`SELECT p.Id, p.Imagen, p.Nombre FROM Personaje as p INNER JOIN PersonajeXPeliculaSerie ON p.Id = PersonajeXPeliculaSerie.IdPersonaje INNER JOIN PeliculaSerie ON PersonajeXPeliculaSerie.IdPeliculaSerie = PeliculaSerie.Id WHERE PersonajeXPeliculaSerie.IdPeliculaSerie = @whereCondition2 AND p.Nombre LIKE '%${nombre}%'`)
+    query = personaje.IdPelicula ? 
+        'SELECT Personajes.Imagen, Personajes.Nombre, Personajes.IDPersonaje FROM Peliculas inner join Conexiones on Peliculas.IDPelicula = Conexiones.IDPelicula inner join Personajes on Conexiones.IDPersonaje = Personajes.IDPersonaje Where Peliculas.IDPelicula = @pIdMovie and'
+        : 'SELECT Personajes.Imagen, Personajes.Nombre, Personajes.IDPersonaje FROM Personajes Where'
 
-            }
-            else{
-                results = await conn.request().input("whereCondition", nombre).query(`SELECT Id, Imagen, Nombre FROM Personaje WHERE Personaje.Nombre LIKE '%${nombre}%'`)
-
-            }
-        }
+/*     if (personaje.IdPelicula) {
+        query = 'SELECT Personajes.Imagen, Personajes.Nombre, Personajes.IDPersonaje FROM Peliculas inner join Conexiones on Peliculas.IDPelicula = Conexiones.IDPelicula inner join Personajes on Conexiones.IDPersonaje = Personajes.IDPersonaje Where Peliculas.IDPelicula = @pIdMovie and';
     }
-    else if(edad){
-        if(movie)
-        {
-            results = await conn.request().input("whereCondition", edad).input("whereCondition2", movie).query('SELECT p.Id, p.Imagen, p.Nombre FROM Personaje as p INNER JOIN PersonajeXPeliculaSerie ON p.Id = PersonajeXPeliculaSerie.IdPersonaje INNER JOIN PeliculaSerie ON PersonajeXPeliculaSerie.IdPeliculaSerie = PeliculaSerie.Id WHERE PersonajeXPeliculaSerie.IdPeliculaSerie = @whereCondition2 AND p.Edad = @whereCondition')
-
-        }
-        else
-        {
-            results = await conn.request().input("whereCondition", edad).query('SELECT Id, Imagen, Nombre FROM Personaje WHERE Personaje.Edad = @whereCondition')
-        }
+    else {
+        query = 'SELECT Personajes.Imagen, Personajes.Nombre, Personajes.IDPersonaje FROM Personajes Where';
+    } */
+    if (personaje.Nombre) {
+        query = query + ' Personajes.Nombre = @pName and';
     }
-    else if(movie)
-    {
-        results = await conn.request().input("whereCondition", movie).query("SELECT p.Id, p.Imagen, p.Nombre FROM Personaje as p INNER JOIN PersonajeXPeliculaSerie ON p.Id = PersonajeXPeliculaSerie.IdPersonaje INNER JOIN PeliculaSerie ON PersonajeXPeliculaSerie.IdPeliculaSerie = PeliculaSerie.Id WHERE PersonajeXPeliculaSerie.IdPeliculaSerie = @whereCondition")
+    if (personaje.Edad) {
+        query = query + ' Personajes.Edad = @pAge and';
     }
-    else
-    {
-        results = await conn.request().query('SELECT Id, Imagen, Nombre FROM Personaje')
+    if (personaje.Peso) {
+        query = query + ' Personajes.Peso = @pWeight and';
     }
-
-    console.log(results)
-    return results;
+    query = query.slice(0, -4);
+    results = await conn.request().input('pName', sql.VarChar, personaje.Nombre).input('pAge', sql.Int, personaje.Edad).input('pWeight', sql.Float, personaje.Peso).input('pIdMovie', sql.Int, personaje.IdPelicula).query(query);
+    return results.recordset;
 }
 
-export const getByID = async (numero) => {
+export const getAllCharacters = async () => {
     const conn = await sql.connect(configDB);
-    const results = await conn.request().input("whereCondition", numero).query("SELECT p.Id, p.Imagen, p.Nombre, p.Edad, p.Peso , p.Historia, STRING_AGG(ps.Titulo + ' (Poster: ' + ps.Imagen + ' Fecha de estreno: ' + Convert(VARCHAR(MAX),ps.FechaCreacion) + ' Calificacion: ' + Convert(VARCHAR(MAX),ps.Calificacion) + ')' , ';') as Peliculas FROM Personaje AS p INNER JOIN PersonajeXPeliculaSerie as pxs ON p.Id = pxs.IdPersonaje INNER JOIN PeliculaSerie AS ps ON pxs.IdPeliculaSerie = ps.Id WHERE p.Id = @whereCondition GROUP BY p.Id, p.Imagen, p.Nombre, p.Edad, p.Peso , p.Historia");
-    if(results.recordset[0] != undefined){
-        results.recordset[0].Peliculas = results.recordset[0].Peliculas.split(';')
+    const results = await conn.request().query('SELECT Personajes.Imagen, Personajes.Nombre, Personajes.IDPersonaje FROM Personajes');
+    return results.recordset;
+}
+
+export const createCharacter = async (personaje) => {
+    const conn = await sql.connect(configDB);
+    const results = await conn.request().input('pImagen', sql.VarChar, personaje.Imagen).input('pNombre', sql.VarChar, personaje.Nombre).input('pEdad', sql.Int, personaje.Edad).input('pPeso', sql.Float, personaje.Peso).input('pHistoria', sql.VarChar, personaje.Historia).query('INSERT INTO Personajes (Imagen, Nombre, Edad, Peso, Historia) VALUES (@pImagen, @pNombre, @pEdad, @pPeso, @pHistoria)');
+    return results.rowsAffected;
+}
+
+export const updateCharacter = async (personaje, id) => {
+    const conn = await sql.connect(configDB);
+    const results = await conn.request().input('pId', sql.Int, id).input('pNombre', sql.VarChar, personaje.Nombre).input('pimagen', sql.VarChar, personaje.Imagen).input('pEdad', sql.Int, personaje.Edad).input('pPeso', sql.Float, personaje.Peso).input('pHistoria', sql.VarChar, personaje.Historia).query('UPDATE Personajes SET Imagen=@pImagen, Nombre=@pNombre, Edad=@pEdad, Peso=@pPeso, Historia = @pHistoria WHERE IDPersonaje=@pId');
+    return results.rowsAffected;
+}
+
+export const deleteCharacter = async (id) => {
+    const conn = await sql.connect(configDB);
+    const results1 = await conn.request().input('pId', sql.Int, id).query('DELETE FROM Conexiones where IDPersonaje = @pId');
+    const results2 = await conn.request().input('pId', sql.Int, id).query('DELETE FROM Personajes where IDPersonaje = @pId');
+    return results1.recordset;
+}
+
+export const getDetailedCharacter = async (id) => {
+    const conn = await sql.connect(configDB);
+    const results1 = await conn.request().input('pId', sql.Int, id).query('select * from Personajes where IDPersonaje = @pId;');
+    const results2 = await conn.request().input('pId', sql.Int, id).query('select Peliculas.* from Peliculas inner join Conexiones on Peliculas.IDPelicula = Conexiones.IDPelicula inner join Personajes on Conexiones.IDPersonaje = Personajes.IDPersonaje where Personajes.IDPersonaje = @pId;');
+    if (results1.recordset[0] === undefined) {
+        return null;
     }
-    console.log(results)
-    return results
+    results1.recordset[0].Peliculas = results2.recordset;
+    return results1.recordset[0];
 }
-
-export const getByIDSinUnion = async (numero) => {
-    const conn = await sql.connect(configDB);
-    const results = await conn.request().input("whereCondition", numero).query('SELECT * FROM Personaje WHERE Personaje.Id = @whereCondition');
-    console.log(results)
-    return results
-}
-
-export const create = async (personaje) =>{
-    const conn = await sql.connect(configDB);
-    await conn.request()
-    .input("pNombre", personaje.nombre)
-    .input("pImagen", personaje.imagen)
-    .input("pEdad", personaje.edad)
-    .input("pPeso", personaje.peso)
-    .input("pHistoria", personaje.historia)
-    .query('INSERT INTO Personaje (Imagen, Nombre, Edad, Peso, Historia) VALUES (@pImagen, @pNombre, @pEdad, @pPeso, @pHistoria)');
-} 
-
-
-export const deleteByID = async(numero) =>{
-    const conn = await sql.connect(configDB);
-    await conn.request().input("whereCondition", numero).query('DELETE FROM Personaje WHERE Personaje.Id = @whereCondition'); 
-}
-
-export const update = async (id, personaje) =>{
-    const conn = await sql.connect(configDB);
-    await conn.request()
-    .input("whereCondition", id)
-    .input("pNombre", personaje.nombre)
-    .input("pImagen", personaje.imagen)
-    .input("pEdad", personaje.edad)
-    .input("pPeso", personaje.peso)
-    .input("pHistoria", personaje.historia)
-    .query('UPDATE Personaje SET Imagen = @pImagen, Nombre = @pNombre, Edad = @pEdad, Peso = @pPeso, Historia = @pHistoria WHERE Personaje.Id LIKE @whereCondition');
-
-}
-
