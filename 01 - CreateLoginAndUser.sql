5515c870e50db4d489626ce2055388c46ba8d59b
komang234 <116166626+komang234@users.noreply.github.com>
2023-06-22 01:18:22 -0300

    Final

--- 01 - CreateLoginAndUser.sql
+++ 01 - CreateLoginAndUser.sql
@@ -1,13 +1,1 @@
-USE [master]
-GO
-CREATE LOGIN [Personaje] WITH PASSWORD=N'Personajes', DEFAULT_DATABASE=[PersonajeYPeliculas], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
-GO
 
-USE [PersonajeYPeliculas]
-GO
-CREATE USER [Personaje] FOR LOGIN [Personaje]
-GO
-USE [PersonajeYPeliculas]
-GO
-ALTER ROLE [db_owner] ADD MEMBER [Personaje]
-GO
